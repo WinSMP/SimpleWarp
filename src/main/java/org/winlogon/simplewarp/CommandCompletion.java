fe7ec3d43e1a9961e688b5ba719c1eda2799fb60
winlogon <walker84837@gmail.com>
2025-03-26 12:51:30 +0100

    chore: use use newer Java features (such as var)

--- src/main/java/org/winlogon/simplewarp/CommandCompletion.java
+++ src/main/java/org/winlogon/simplewarp/CommandCompletion.java
@@ -5,46 +5,48 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import com.github.walker84837.JResult.Result;
-
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommandCompletion implements TabCompleter {
     private final DatabaseHandler databaseHandler;
+    private final ChatColor cc;
 
-    public CommandCompletion(DatabaseHandler databaseHandler) {
+    public CommandCompletion(DatabaseHandler databaseHandler, ChatColor chatFormatter) {
         this.databaseHandler = databaseHandler;
+        this.cc = chatFormatter;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        List<String> suggestions = new ArrayList<>();
-
         if (args.length == 1) {
-            suggestions.add("new");
-            suggestions.add("remove");
-            suggestions.add("edit");
-            suggestions.add("teleport");
-            suggestions.add("tp");
-        } else if (args.length == 2 && !args[0].equalsIgnoreCase("new")) {
-            Result<Connection, Exception> conn = databaseHandler.getConnection();
-            conn.ifErr(e -> Bukkit.getLogger().severe("Failed to get database connection: " + e.getMessage()));
-
+            return new ArrayList<>(List.of("new", "remove", "edit", "teleport", "tp"));
+        }
+        
+        var suggestions = new ArrayList<String>();
+        
+        if (args.length == 2 && !args[0].equalsIgnoreCase("new")) {
+            var conn = databaseHandler.getConnection();
+            conn.ifErr(e ->
+                Bukkit.getLogger().severe("Failed to get database connection: " + e.getMessage())
+            );
+        
             try (Connection connection = conn.unwrap();
-                Statement stmt = connection.createStatement()) {
-                ResultSet rs = stmt.executeQuery("SELECT name FROM warps");
+                 var stmt = connection.createStatement();
+                 var rs = stmt.executeQuery("SELECT name FROM warps")) {
+                
                 while (rs.next()) {
                     suggestions.add(rs.getString("name"));
                 }
             } catch (SQLException e) {
-                sender.sendMessage("<red>Failed to fetch warp names for tab completion: <gray>" + e.getMessage());
+                sender.sendMessage(
+                    cc.format("<red>Failed to fetch warp names for tab completion: <gray>" + e.getMessage())
+                );
             }
         }
+        
         return suggestions;
     }
 }
