2dfe4255fbf273a09087b599ab04ba00578767dc
winlogon <walker84837@gmail.com>
2025-03-26 22:46:27 +0100

    chore: use features from new versions of java

--- src/main/java/org/winlogon/simplewarp/WarpPlugin.java
+++ src/main/java/org/winlogon/simplewarp/WarpPlugin.java
@@ -1,16 +1,10 @@
 package org.winlogon.simplewarp;
 
-// import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import dev.jorel.commandapi.CommandAPI;
-// import dev.jorel.commandapi.CommandAPIConfig;
-// import java.sql.SQLException;
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.concurrent.CompletableFuture;
 
 public class WarpPlugin extends JavaPlugin {
-    public static boolean IS_FOLIA = checkFolia();
+    public static final boolean IS_FOLIA = checkFolia();
     public static DatabaseHandler databaseHandler;
     public static ChatColor cc;
 
@@ -28,44 +22,12 @@ public class WarpPlugin extends JavaPlugin {
         CommandAPI.onEnable();
         saveDefaultConfig();
         cc = new ChatColor();
-        databaseHandler = new DatabaseHandler(this);
 
+        databaseHandler = new DatabaseHandler(this);
         databaseHandler.connectToDatabase().ifErr(e -> {
-            getLogger().severe("Failed to initialize database: " + e.getMessage());
+            getLogger().severe(() -> "Failed to initialize database: " + e.getMessage());
             getServer().getPluginManager().disablePlugin(this);
         });
-
-        // Register 'warps' suggestion provider
-        //CommandAPI.register("warps", info -> {
-        //    CompletableFuture<String[]> future = new CompletableFuture<>();
-
-        //    Runnable task = () -> {
-        //        List<String> suggestions = new ArrayList<>();
-        //        var connResult = databaseHandler.getConnection();
-        //        if (!connResult.isOk()) {
-        //            future.complete(new String[0]);
-        //            return;
-        //        }
-        //        try (var conn = connResult.unwrap();
-        //             var stmt = conn.createStatement();
-        //             var rs = stmt.executeQuery("SELECT name FROM warps")) {
-        //            while (rs.next()) {
-        //                suggestions.add(rs.getString("name"));
-        //            }
-        //            future.complete(suggestions.toArray(new String[0]));
-        //        } catch (SQLException e) {
-        //            future.complete(new String[0]);
-        //        }
-        //    };
-
-        //    if (IS_FOLIA) {
-        //        Bukkit.getScheduler().runTaskAsynchronously(this, task);
-        //    } else {
-        //        Bukkit.getScheduler().runTaskAsynchronously(this, task);
-        //    }
-
-        //    return future;
-        //});
 
         CommandAPI.registerCommand(WarpCommand.class);
     }
@@ -73,7 +35,7 @@ public class WarpPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         var result = databaseHandler.closeConnection();
-        result.ifErr(e -> getLogger().severe("Failed to close connection: " + e.getMessage()));
+        result.ifErr(e -> getLogger().severe(() -> "Failed to close connection: " + e.getMessage()));
         CommandAPI.onDisable();
     }
 }
